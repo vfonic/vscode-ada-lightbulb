b49aa237bd292e08e8a962718467b7adeae076ec
Viktor Fonic <viktor.fonic@gmail.com>
2019-05-09 20:32:30 +0700

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,25 +11,27 @@ module.exports = {
     },
     sourceType: 'module'
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  extends: ['airbnb-base', 'eslint:recommended', 'plugin:prettier/recommended'],
   plugins: ['prettier'],
   rules: {
+    'brace-style': ['error', '1tbs'],
+    'constructor-super': 'error',
+    curly: ['error', 'all'],
     eqeqeq: ['error', 'always', { null: 'ignore' }],
+    'linebreak-style': ['error', 'unix'],
+    'max-len': ['error', { code: 120, ignoreUrls: true }],
     'no-const-assign': 'error',
+    'no-tabs': 'error',
     'no-this-before-super': 'error',
     'no-throw-literal': 'error',
     'no-undef': 'error',
     'no-unreachable': 'error',
     'no-unused-expressions': 'error',
-    'constructor-super': 'error',
-    'valid-typeof': 'error',
-    'prettier/prettier': 'error',
     'no-extra-boolean-cast': 0,
     'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: true }],
     'no-debugger': 'error',
-    curly: ['error', 'all'],
-    'brace-style': ['error', '1tbs'],
-    'linebreak-style': ['error', 'unix'],
-    'sort-imports': 'error'
+    'prettier/prettier': ['error', { singleQuote: true }],
+    'sort-imports': 'error',
+    'valid-typeof': 'error'
   }
 };
